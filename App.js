b6dc9d7b4eaf1f84843d103afe66f40a3df9f2ef
haroonkhan29 <haroon77.afridi@gmail.com>
2024-06-14 22:17:56 +0500

    update

--- App.js
+++ App.js
@@ -28,8 +28,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://admin2:admin2@localhost:27017/quix-db?authSource=admin');
-
+mongoose.connect('mongodb://127.0.0.1:27017/quix-db', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use('/bank', bankRoutes);
 app.use('/employee', employeeRoutes);
 app.use('/attendance', attendanceRoutes);
